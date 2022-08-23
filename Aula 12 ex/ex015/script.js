f845d2b979e09f1284cee44707efe7170102073a
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-08-23 13:46:58 -0300

    ex15

---
+++ Aula 12 ex/ex015/script.js
@@ -0,0 +1,22 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] insira os dados novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked)
+            genero = 'Mulher'
+
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+}
