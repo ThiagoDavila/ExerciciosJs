f492be64c263b2bf82546da62aa34d76377b3b0d
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-08-25 10:46:43 -0300

    Update script.js

--- Aula 12 ex/ex015/script.js
+++ Aula 12 ex/ex015/script.js
@@ -10,13 +10,41 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = document.createElement('img')
-        img.setAttribute('id','foto')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
-        } else if (fsex[1].checked)
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'img/bebehomem.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'img/jovemhomem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'img/adultohomem.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'img/idosohomem.png')
+            }
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'img/bebemulher.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'img/jovemmulher.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'img/adultomulher.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'img/idosomulher.png')
+            }
 
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
-    res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
 }
