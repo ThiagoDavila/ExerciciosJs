956882a9f2d22eba59fc30544e10b0de386d87aa
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-09-13 11:19:42 -0300

    ex 16

--- Aula 16 ex/Ex 18/script.js
+++ Aula 16 ex/Ex 18/script.js
@@ -18,9 +18,48 @@ function inLista(n, l) {
     }
 }
 function adicionar() {
+    // ! significa não em JS
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-
+       valores.push(Number(num.value))
+       //adicionar itens dentro do select
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado`
+       // Para colocar na lista
+       lista.appendChild(item)
+       //res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    /* Para apagar um numero dentro da caixa de adicionar valores*/
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicionar valores antes de finalizar!')
+    }else{
+        //Para mostrando quantos numeros cadastrados
+        let tot = valores.length
+        //maior e menor valores
+        let maior = valores[0]
+        let menor = valores[0]
+        //media
+        let soma = 0
+        let media = 0
+        for( let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor )
+                maior = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p> Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML +=`<p> A média dos valores digitados é ${media}</p>`
+    }
+
 }
